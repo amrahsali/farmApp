60d3835f066794ce05bd3ad4d4f2c6415ab0429e
amrah <Saliamrah300@gmail.com>
2022-10-11 10:08:39 +0100

    inter-page

--- app/src/main/java/com/example/farmapp/WalletFragment.java
+++ app/src/main/java/com/example/farmapp/WalletFragment.java
@@ -1,5 +1,6 @@
 package com.example.farmapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class WalletFragment extends Fragment {
 
-    public WalletFragment(){
+    public WalletFragment() {
         // require a empty public constructor
     }
-
+   Button button;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        button.findViewById(R.id.withdraw);
+//        button.setOnClickListener(
+//
+//        );
         return inflater.inflate(R.layout.fragment_wallet, container, false);
+
+
     }
+
+
+
+
 }
