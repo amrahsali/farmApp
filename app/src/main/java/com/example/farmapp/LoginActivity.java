63311f439fd02d64542dd350e0129fd15aad1a0e
amrah <Saliamrah300@gmail.com>
2022-10-28 11:15:53 +0100

    create a login fragment for farmers and users. add a checkbox for language create a theme for language

--- app/src/main/java/com/example/farmapp/LoginActivity.java
+++ app/src/main/java/com/example/farmapp/LoginActivity.java
@@ -1,25 +1,53 @@
 package com.example.farmapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class LoginActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView loginNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginNavigationView = findViewById(R.id.loginNavigationView);
+        loginNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loginNavigationView.setSelectedItemId(R.id.farmers);
 
     }
 
-    public void Login(View view) {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+    FarmersLoginFragment farmers = new FarmersLoginFragment();
+    UsersFragment users = new UsersFragment();
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        switch (item.getItemId()) {
+            case R.id.farmers:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container2, farmers).commit();
+                return true;
+
+            case R.id.users:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container2, users).commit();
+                return true;
+
+        }
+
+        return false;
     }
+
 
     public void TosignUp(View view) {
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
--- app/src/main/java/com/example/farmapp/SignupActivity.java
+++ app/src/main/java/com/example/farmapp/SignupActivity.java
@@ -39,14 +39,14 @@ public class SignupActivity extends AppCompatActivity {
 
         phone = phoneNumber.getText().toString();
 
-        PhoneAuthOptions options =
-                PhoneAuthOptions.newBuilder(mAuth)
-                        .setPhoneNumber(phoneNumber)       // Phone number to verify
-                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-                        .setActivity(this)                 // Activity (for callback binding)
-                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
-                        .build();
-        PhoneAuthProvider.verifyPhoneNumber(options);
+       // PhoneAuthOptions options =
+            //    PhoneAuthOptions.newBuilder(mAuth)
+            //            .setPhoneNumber(phoneNumber)       // Phone number to verify
+           //             .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+           //             .setActivity(this)                 // Activity (for callback binding)
+           //             .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+          //              .build();
+     //   PhoneAuthProvider.verifyPhoneNumber(options);
 
     }
 }
