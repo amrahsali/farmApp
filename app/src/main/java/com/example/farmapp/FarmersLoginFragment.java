f0eb3817582e0ea7ea1cdf678fc98cec99e2da86
amrah <Saliamrah300@gmail.com>
2022-11-01 13:13:43 +0100

    add strings of each statement fixed users and farmers fragment

--- app/src/main/java/com/example/farmapp/FarmersLoginFragment.java
+++ app/src/main/java/com/example/farmapp/FarmersLoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.farmapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class FarmersLoginFragment extends Fragment {
+ Button button;
 
     public FarmersLoginFragment() {
         // require a empty public constructor
+
 
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container2, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_farmers_login, container2, false);
+
+        View view = inflater.inflate(R.layout.fragment_farmers_login, container2, false);
+        button = view.findViewById(R.id.farmersbtn);
+       button.setOnClickListener(new View.OnClickListener() {
+
+
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getActivity(), MainActivity.class);
+               startActivity(intent);
+           }
+       });
+        return view;
+
     }
 }
--- app/src/main/java/com/example/farmapp/UsersFragment.java
+++ app/src/main/java/com/example/farmapp/UsersFragment.java
@@ -1,5 +1,6 @@
 package com.example.farmapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,9 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class UsersFragment extends Fragment {
+
+    Button button;
     public UsersFragment() {
         // require a empty public constructor
 
@@ -17,6 +22,21 @@ public class UsersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container2, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_login, container2, false);
+
+        View view = inflater.inflate(R.layout.fragment_users_login, container2, false);
+        
+        button = view.findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(), "User's dashboard not available", Toast.LENGTH_SHORT).show();
+            //    Intent intent = new Intent(getActivity(), MainActivity.class);
+              //  startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
