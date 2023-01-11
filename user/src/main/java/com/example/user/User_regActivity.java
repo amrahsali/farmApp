ede768a7aa193039841488a904095a1da146874a
amrah <Saliamrah300@gmail.com>
2023-01-11 13:26:36 +0100

    User

--- user/src/main/java/com/example/user/User_regActivity.java
+++ user/src/main/java/com/example/user/User_regActivity.java
@@ -3,18 +3,38 @@ package com.example.user;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class User_regActivity extends AppCompatActivity  implements BottomNavigationView.OnNavigationItemSelectedListener {
-
+public class User_regActivity extends AppCompatActivity  implements BottomNavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener {
+    String[] languages = { "select language", "english", "hausa" };
+    Context context;
+    Resources resources;
     BottomNavigationView regNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_reg);
+
+
+        Spinner spin = findViewById(R.id.spinnerLang);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, languages);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
 
         regNavigationView = findViewById(R.id.regNavigationView);
         regNavigationView.setOnNavigationItemSelectedListener(this);
@@ -39,7 +59,36 @@ public class User_regActivity extends AppCompatActivity  implements BottomNaviga
                 getSupportFragmentManager().beginTransaction().replace(R.id.usercontainer1, usersignup).commit();
                 return true;
         }
-
         return false;
     }
-}
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        if (languages[position].equals("hausa")){
+            context = LocaleHelper.setLocale(User_regActivity.this, "ha");
+            resources = context.getResources();
+            Intent refresh = new Intent(context, User_regActivity.class);
+            Toast.makeText(getApplicationContext(),languages[position] , Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(refresh);
+        }else if(languages[position].equals("english")){
+            //Toast.makeText( LoginActivity.this, "checkbox is unchecked", Toast.LENGTH_SHORT).show();
+            context = LocaleHelper.setLocale(User_regActivity.this, "en");
+            resources = context.getResources();
+            Intent refresh = new Intent(context, User_regActivity.class);
+            Toast.makeText(getApplicationContext(),languages[position] , Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(refresh);
+        }else {
+            Toast.makeText(getApplicationContext(),"languages" , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+            Toast.makeText(getApplicationContext(),"languages" , Toast.LENGTH_SHORT).show();
+
+
+        }
+
+    }
--- user/src/main/java/com/example/user/userLoginFragment.java
+++ user/src/main/java/com/example/user/userLoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -24,6 +25,23 @@ public class userLoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_login, container, false);
+
+
+        button = view.findViewById(R.id.loginBtn);
+        button.setOnClickListener(
+
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+        return  view;
     }
+
+
 }
